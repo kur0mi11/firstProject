7b3f2f6770bbacc25508b5ec7953a98075329e5c
SakinahInaa <sakinahismail15@gmail.com>
2024-10-06 18:28:52 +0700

    Update firstProject.cpp

--- firstProject.cpp
+++ firstProject.cpp
@@ -49,24 +49,62 @@ int main () {
 	break;
 	  }else{
 		switch (pilih[i]){
-case 1 : menu[i] = "Es Kul - Kul"; harga[i] = 2000; break;
-case 2 : menu[i] = "Dimsum"; harga[i] = 1000; break;
-case 3 : menu[i] = "Roti Bakar"; harga[i] = 5000; break;
-case 4 : menu[i] = "Siomay"; harga[i] = 5000; break;
-case 5 : menu[i] = "Matcha Latte"; harga[i] = 15000; break;
-case 6 : menu[i] = "Ketan Durian"; harga[i] = 5000; break;
-case 7 : menu[i] = "Burger"; harga[i] = 5000; break;
-case 8 : menu[i] = "Ayam Geprek"; harga[i] = 10000; break;
-case 9 : menu[i] = "Es Ketan Wasii"; harga[i] = 5000; break;
-case 10 : menu[i] = "Nasi Bakar"; harga[i] = 5000; break;
-}
-cout<<"\n";
-cout<<"Mau berapa kawan"<<menu[i]<<"nya?\n";
-cout<<"Aku mau:";
-cin>>jumlah[i];
-cout<<"\n";
+			case 1 : menu[i] = "Es Kul - Kul"; harga[i] = 2000; break;
+			case 2 : menu[i] = "Dimsum"; harga[i] = 1000; break;
+			case 3 : menu[i] = "Roti Bakar"; harga[i] = 5000; break;
+			case 4 : menu[i] = "Siomay"; harga[i] = 5000; break;
+			case 5 : menu[i] = "Matcha Latte"; harga[i] = 15000; break;
+			case 6 : menu[i] = "Ketan Durian"; harga[i] = 5000; break;
+			case 7 : menu[i] = "Burger"; harga[i] = 5000; break;
+			case 8 : menu[i] = "Ayam Geprek"; harga[i] = 10000; break;
+			case 9 : menu[i] = "Es Ketan Wasii"; harga[i] = 5000; break;
+			case 10 : menu[i] = "Nasi Bakar"; harga[i] = 5000; break;
+		}
+		  cout<<"\n";
+		  cout<<"Mau berapa kawan"<<menu[i]<<"nya?\n";
+		  cout<<"Aku mau:";
+		  cin>>jumlah[i];
+		  cout<<"\n";
+	  }
+
+	sub_total[i] = jumlah[i]*harga[i];
+	total_bayar+=sub_total[i];
+	i++;
+
+
+        } else {
+		cout << "Kamu salah tekan ya kawan? \n";
+		cout << "Gapapa! Coba lagi ya ^^ \n";
+		cout << "\n";
+	}
+       }
+	liatStruk(menu, harga, jumlah, sub_total, total_bayar, i);
+	
+	return 0;
 }
 
-sub_total[i] = jumlah[i]*harga[i];
-total_bayar+=sub_total[i];
-i++;
+void liatStruk(string menu[], int harga[], int jumlah[], int sub_total[], int total_bayar, int i) {
+
+cout << "\n";
+
+cout << "=================================================================\n";
+
+cout << "*********************** STRUK PEMBAYARAN ************************\n";
+
+cout << "=================================================================\n";
+
+cout << "NO | NAMA MENU | HARGA/pcs | JUMLAH | SUB TOTAL \n";
+
+cout << "-----------------------------------------------------------------\n";
+
+for (int a = 0; a < i; a++){
+
+cout << a+1 << " | " << menu[a] << " | " << harga[a] << " | " << jumlah[a] << " | " << 
+
+sub_total[a] << endl;
+
+}
+
+cout << "\n";
+
+cout << "-----------------------------------------------------------------\n";
